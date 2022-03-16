12f5582d6103c3de4625a61657be2c3a72d8a3c6
kiranpatilksp <101632387+kiranpatilksp@users.noreply.github.com>
2022-03-16 14:56:52 +0530

    Add files via upload

---
+++ Swap2.java
@@ -0,0 +1,17 @@
+package Inclass;
+
+public class Swap2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		System.out.println(" before  swap ="+ a+ "  " + b);
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println(" after  swap ="+ a+ "  " + b);
+
+	}
+
+}
